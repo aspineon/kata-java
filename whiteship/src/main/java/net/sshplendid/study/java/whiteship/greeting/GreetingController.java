fb148c2f117d769f578a58e872a82b2268927e73
Shin, SeHun <shun.shin@samsung.com>
2018-11-13 13:53:02 +0900

    create greeting controller
    greeting 기능 구현(without parameter)

---
+++ whiteship/src/main/java/net/sshplendid/study/java/whiteship/greeting/GreetingController.java
@@ -0,0 +1,13 @@
+package net.sshplendid.study.java.whiteship.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+}
---
+++ whiteship/src/test/java/net/sshplendid/study/java/whiteship/greeting/GreetingControllerTest.java
@@ -0,0 +1,25 @@
+package net.sshplendid.study.java.whiteship.greeting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class GreetingControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void test_getGreeting_withoutParameter() throws Exception {
+        this.mockMvc.perform(get("/greeting"))
+        .andExpect(status().isOk());
+    }
+}
