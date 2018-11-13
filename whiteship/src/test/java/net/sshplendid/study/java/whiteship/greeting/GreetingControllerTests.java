8f69f601cd87a034b034b5024276b3353f29c2a4
Shin, SeHun <shun.shin@samsung.com>
2018-11-13 14:42:40 +0900

    refactor: Controller테스트 리팩토링 boiler plate는 CommonControllerTests로 추출

--- whiteship/src/test/java/net/sshplendid/study/java/whiteship/greeting/GreetingControllerTests.java
+++ whiteship/src/test/java/net/sshplendid/study/java/whiteship/greeting/GreetingControllerTests.java
@@ -1,25 +1,14 @@
 package net.sshplendid.study.java.whiteship.greeting;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
-@WebMvcTest
-public class GreetingControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
+public class GreetingControllerTests extends CommonControllerTests {
 
     @Test
     public void test_getGreeting_withoutParameter() throws Exception {
