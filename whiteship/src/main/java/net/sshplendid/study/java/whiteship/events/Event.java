3c62b0bdbbbf248a1d2675f14857636f0e5cc32f
Shin, SeHun <shun.shin@samsung.com>
2018-11-13 15:10:21 +0900

    Event 도메인 enum type 변경

--- whiteship/src/main/java/net/sshplendid/study/java/whiteship/events/Event.java
+++ whiteship/src/main/java/net/sshplendid/study/java/whiteship/events/Event.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
@@ -31,5 +33,6 @@ public class Event {
     private int maxPrice;
     private int limitOfEnrollment;
 
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 }
