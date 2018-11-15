1eb151f5faf152f796d3fe6c56cda8df4ce55330
SeHun Shin <s.shin@marathoner.kr>
2018-11-15 17:18:10 +0900

    create event

--- whiteship/src/main/java/net/sshplendid/study/java/whiteship/events/EventController.java
+++ whiteship/src/main/java/net/sshplendid/study/java/whiteship/events/EventController.java
@@ -2,6 +2,7 @@ package net.sshplendid.study.java.whiteship.events;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class EventController {
     private final AtomicInteger counter = new AtomicInteger();
 
     @PostMapping
-    public ResponseEntity<Event> create(Event body) {
+    public ResponseEntity<Event> create(@RequestBody Event body) {
         body.setId(counter.incrementAndGet());
         return ResponseEntity.ok(body);
     }
