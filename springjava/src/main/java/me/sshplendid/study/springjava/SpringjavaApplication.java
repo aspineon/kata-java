b8ccbdf5bb9d0f43f99cb719365a10e6e37031ab
SeHun Shin <s.shin@marathoner.kr>
2018-11-20 16:04:04 +0900

    chore: spring-kotlin 비교 프로젝트 생성

---
+++ springjava/src/main/java/me/sshplendid/study/springjava/SpringjavaApplication.java
@@ -0,0 +1,12 @@
+package me.sshplendid.study.springjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjavaApplication.class, args);
+	}
+}
